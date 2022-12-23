43925cafc9e8494582fbad4328ccf3162ee757b4
Abhishek Shukla <abhishek@192.168.1.53>
2022-12-23 14:05:59 +0530

    Initial commit

---
+++ src/test/java/com/abhishek/Authentication_JWT/AuthenticationJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.Authentication_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
